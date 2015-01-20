5cbbdb20f7a08314677fd64dd048210d7e191bdf
Cory Alder <coryalder@gmail.com>
2015-01-19 17:08:10 -0800

    Resolves issue #15 temporary images are now deleted immediately once sharing has occurred

--- DMActivityInstagram/DMActivityInstagram.m
+++ DMActivityInstagram/DMActivityInstagram.m
@@ -112,7 +112,8 @@
     if (![self.documentController presentOpenInMenuFromBarButtonItem:self.presentFromButton animated:YES]) NSLog(@"couldn't present document interaction controller");
 }
 
--(void)documentInteractionController:(UIDocumentInteractionController *)controller willBeginSendingToApplication:(NSString *)application {
+
+- (void)documentInteractionController:(UIDocumentInteractionController *)controller didEndSendingToApplication:(NSString *)application {
     [self activityDidFinish:YES];
 }
 
@@ -126,6 +127,13 @@
     return (imageSize.height == imageSize.width);
 }
 
-
+-(void)activityDidFinish:(BOOL)success {
+    NSError *error = nil;
+    NSString *writePath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"instagram.igo"];
+    if (![[NSFileManager defaultManager] removeItemAtPath:writePath error:&error]) {
+        NSLog(@"Error cleaning up temporary image file: %@", error);
+    }
+    [super activityDidFinish:success];
+}
 
 @end
